5d5f503f153fc09d0355067070d3fffca5c1c4a1
jeries khoury <jbk271099@gmail.com>
2020-09-21 20:54:35 +0300

    renovating ui and added pages

--- UserInterface_test.py
+++ UserInterface_test.py
@@ -1,4 +1,4 @@
-from flask import Flask , redirect , url_for,render_template
+from flask import Flask , redirect , url_for,render_template,request
 
 app = Flask(__name__)
 
@@ -8,11 +8,25 @@ def home():
 
 @app.route("/<num_players>")
 def start(num_players):
-    return f"wtf man?, {num_players}"
+    return f"{num_players} your ass get your ass back to home screen"
 
-@app.route('/black')
+@app.route('/description')
 def test():
-    return redirect("https://www.google.com/search?q=why+are+you+so+horney&rlz=1C1CHBF_enUS877US878&oq=why&aqs=chrome.0.69i59j46j35i39j46j0j46l2.2103j0j4&sourceid=chrome&ie=UTF-8")
+    return redirect("https://monopolynerd.com/online-monopoly-simulator/")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/pornhub')
+def pornhub():
+    return redirect("https://me.me/i/why-are-you-that-horny-9615a7eeb02f4dd89d653089991f7f66")
+
+@app.route('/play')
+def play():
+    return render_template("play.html")
+
+
 
 
 if __name__ == "__main__":
--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import Board as b
 import  destinations as d
 import  cards as c
-
+ 
 s = input("please enter number of players between 3 and 6:")
 while True:
     if int(s) > 6 or int(s) < 2:
