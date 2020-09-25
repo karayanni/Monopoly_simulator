b71871753438bc6ae933c62adfe421150a05b61c
jeries khoury <jbk271099@gmail.com>
2020-09-25 18:25:49 +0300

    added animations in ui

--- game.py
+++ game.py
@@ -1,14 +1,9 @@
 import Board as b
 import  destinations as d
 import  cards as c
-#import cgi
-#import cgitb
-#
-#form = cgi.FieldStorage()
-#
-#num_of_players = form.getvalue("num_of_players")
-#num_of_steps = form.getvalue("num_of_steps")
-#starting_cash = form.getvalue("starting_cash")
+import cgi
+import cgitb
+
 
 s = input("please enter number of players between 3 and 6:")
 while True:
