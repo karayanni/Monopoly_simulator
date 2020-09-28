ea795db4b172181449b07452a76a145cf6a97175
jeries khoury <jbk271099@gmail.com>
2020-09-28 20:40:06 +0300

    minor fixes

--- UserInterface_test.py
+++ UserInterface_test.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/<num_players>")
-def start(num_players):
-    return f"{num_players} zb"
 
 @app.route("/about")
 def about():
@@ -27,8 +24,7 @@ def get_num_of_steps():
     starting_cash = request.form['starting_cash']
     num_of_players = request.form.get('selected_num_players')
     print(num_of_steps,starting_cash)
-    return render_template("monopoly.html")
-    #return render_template("test.html",num_of_steps=num_of_steps,starting_cash=starting_cash,num_of_players=num_of_players)
+    return render_template("home.html")
 
 
 
