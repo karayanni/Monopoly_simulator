18fecc997a2f86859f2ac4dd82284aa6c503514e
jeries khoury <jbk271099@gmail.com>
2020-09-28 19:30:45 +0300

    fixed input problem and fixed some buttons in ui

--- UserInterface_test.py
+++ UserInterface_test.py
@@ -1,4 +1,5 @@
 from flask import Flask , redirect , url_for,render_template,request
+from time import sleep
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def home():
 
 @app.route("/<num_players>")
 def start(num_players):
-    return f"{num_players} your ass get your ass back to home screen"
+    return f"{num_players} zb"
 
 @app.route("/about")
 def about():
@@ -20,6 +21,16 @@ def simulate():
 
 
 
+@app.route('/simulate',methods = ['POST'])
+def get_num_of_steps():
+    num_of_steps = request.form['num_of_steps']
+    starting_cash = request.form['starting_cash']
+    num_of_players = request.form.get('selected_num_players')
+    print(num_of_steps,starting_cash)
+    return render_template("test.html",num_of_steps=num_of_steps,starting_cash=starting_cash,num_of_players=num_of_players)
+
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
