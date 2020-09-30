71becff3ad7cd5aca384119a45d4063c150e8892
jeries khoury <jbk271099@gmail.com>
2020-09-30 20:55:54 +0300

    added simulation page and some new animations

--- UserInterface_test.py
+++ UserInterface_test.py
@@ -1,5 +1,5 @@
 from flask import Flask , redirect , url_for,render_template,request
-
+from time import sleep
 
 app = Flask(__name__)
 
@@ -16,18 +16,32 @@ def about():
 def simulate():
     return render_template("simulate.html")
 
+@app.route("/img")
+def img():
+    return render_template("monopoly.html")
+
+@app.route("/exit")
+def exit():
+    return render_template("exit.html")
 
 
 @app.route('/simulate',methods = ['POST'])
 def get_num_of_steps():
+    num_of_players = request.form.get('selected_num_players')
     num_of_steps = request.form['num_of_steps']
     starting_cash = request.form['starting_cash']
-    num_of_players = request.form.get('selected_num_players')
     print(num_of_steps,starting_cash)
-    return render_template("home.html")
+    return render_template("monopoly.html",num_of_players=num_of_players,num_of_steps=num_of_steps)
+
+
+@app.route("/test")
+def test():
+    return render_template("temp.html",bedat=temp)
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+f"{jdj} dhhd"
